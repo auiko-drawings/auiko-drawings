a4befb5f00ac6cc1f6c424338c1e03d99e575066
vere <verec@vere.local>
2026-07-21 00:27:07 -0400

    adding project to repository

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import sk1 from "./assets/sk1.jpg";
+import sk2 from "./assets/sk2.jpg";
+
+
+function PhotoCard ({title, imageUrl, text}) {
+  return (
+    <div style ={{ margin: "20px auto ", textAlign: "center", maxWidth:"300px", width:"90%"}}>
+    
+        <img src={imageUrl} alt={title} style={{ width: "100%", height:"auto", borderRadius: "8px"}}/>
+        <p>{text}</p>
+        <h3>{title}</h3>
+    </div>
+  );
+}
+
+function App() {
+  const photos = [
+    {title: "colors", imageUrl:sk1, text:"this is text"},
+    {title: "boxes", imageUrl:sk2, text:"this is text, it should be really in the center" },
+    {title: "souls", imageUrl:"", text: ""}
+  ];
+
+return (
+  <div>
+    <h1>post from something important</h1>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center"}}>
+      {photos.map((photo, index)=>(
+        <PhotoCard key={index} 
+        title={photo.title} 
+        imageUrl={photo.imageUrl} 
+        text = {photo.text}/>
+      ))}
+    </div>
+  </div>
+);
+
+
+}
+
+
+
+export default App;
